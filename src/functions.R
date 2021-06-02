217869e0c5b4e3f05c5c0356009b66a3467ab094
orionzhou <zhoupenggeni@gmail.com>
2021-06-01 20:58:46 -0500

    added wheat xerf

--- src/functions.R
+++ src/functions.R
@@ -9,6 +9,8 @@ gts6 = c("B73",'Mo17','W22','B73xMo17','W22xB73','W22xMo17')
 gts25 = str_split("B73 B97 CML322 CML333 CML52 CML69 DK105 EP1 F7 Il14H Ki11 Ki3 M162W M37W Mo17 Mo18W MS71 NC350 NC358 Oh43 Oh7B P39 PH207 Tx303 W22", " ")[[1]]
 gts31 = str_split("Mo17 W22 B97 CML52 CML69 CML103 CML228 CML247 CML277 CML322 CML333 HP301 Il14H Ki3 Ki11 Ky21 M37W M162W Mo18W Ms71 NC350 NC358 Oh7B Oh43 P39 Tx303 Tzi8 EP1 DK105 F7 PE0075", " ")[[1]]
 gts31_ph207 = str_split("Mo17 W22 PH207 B97 CML52 CML69 CML103 CML228 CML247 CML277 CML322 CML333 HP301 Il14H Ki3 Ki11 Ky21 M37W M162W Mo18W Ms71 NC350 NC358 Oh7B Oh43 P39 Tx303 Tzi8 EP1 DK105 F7 PE0075", " ")[[1]]
+fi = glue("{dirg}2/wheat26.txt")
+gts_wheat26 = read_tsv(fi, col_names='gt') %>% pull(gt)
 
 infer_intron <- function(td, zero=T) { # sorted by chrom and start
     #{{{
--- src/ge.10.xr.R
+++ src/ge.10.xr.R
@@ -14,14 +14,25 @@ read_tsv(fi, col_names=c('tid1','tid2')) %>%
     select(gid1, gid2, type, tid1, tid2)
     #}}}
 }
-read_synmap <- function(qry, tgt='B73', diri='~/projects/wgc/data/raw') {
+read_synmap <- function(qry, tgt='B73', maize=T, diri='~/projects/wgc/data/raw') {
     #{{{
-    fi = glue("{diri}/Zmays_{qry}-Zmays_{tgt}/xref.t.tsv")
-    read_tsv(fi, col_names=c('tid1','tid2', 'type')) %>%
-        filter(tid2 != '.') %>%
-        separate(tid1, c('gid1','iso1'), sep="[\\.\\_]", remove=F) %>%
-        separate(tid2, c('gid2','iso2'), sep="[\\.\\_]", remove=F) %>%
-        select(gid1, gid2, type, tid1, tid2)
+    fi = ifelse(maize, glue("{diri}/Zmays_{qry}-Zmays_{tgt}/xref.t.tsv"),
+                glue("{diri}/{qry}-{tgt}/xref.t.tsv"))
+    ti = read_tsv(fi, col_names=c('tid1','tid2', 'type')) %>%
+        filter(tid2 != '.')
+    if (qry == 'Atauschii_AS60') {
+        ti = ti %>% separate(tid2, c('iso2','gid2'), sep="[\\.]", remove=F)
+    } else if (str_detect(qry, '^Atauschii_')) {
+        ti = ti %>% separate(tid2, c('gid2','iso2'), sep="[\\.]", remove=F)
+    } else {
+        ti = ti %>% separate(tid2, c('gid2','iso2'), sep="[\\.\\_]", remove=F)
+    }
+    if (tgt == 'Atauschii_AS60') {
+        ti = ti %>% separate(tid1, c('iso1','gid1'), sep="[\\.]", remove=F)
+    } else {
+        ti = ti %>% separate(tid1, c('gid1','iso1'), sep="[\\.\\_]", remove=F)
+    }
+    ti %>% select(gid1, gid2, type, tid1, tid2)
     #}}}
 }
 #}}}
@@ -50,12 +61,12 @@ write_tsv(xref, fo, na='')
 #}}}
 
 
-#{{{ syntelog xref table
+#{{{ syntelog xref table - maize
 v = 'v5'
 gt0 = ifelse(v == 'v4', 'B73', 'B73v5')
 qrys = gts31_ph207
 to = tibble(qry=qrys, tgt=gt0) %>%
-    mutate(xref = map2(qry, tgt, read_synmap)) %>%
+    mutate(xref = map2(qry, tgt, read_synmap, maize=T)) %>%
     unnest(xref)
 to %>% count(qry,tgt,type) %>% spread(type,n) %>% print(n=40)
 
@@ -94,7 +105,47 @@ saveRDS(to, fo)
 #}}}
 #}}}
 
+#{{{ syntelog xref table - wheat
+v = 'csd'
+v = 'as60'
+gt0 = ifelse(v == 'csd', 'Taestivum_D', 'Atauschii_AS60')
+qrys = gts_wheat26[!gts_wheat26 %in% gt0]
+to = tibble(qry=qrys, tgt=gt0) %>%
+    mutate(xref = map2(qry, tgt, read_synmap, maize=F)) %>%
+    unnest(xref)
+to %>% count(qry,tgt,type) %>% spread(type,n) %>% print(n=40)
 
+fo = glue('{dirw}/xref.wheat.{v}.tsv')
+write_tsv(to, fo)
 
+#{{{ make xref gene model tibble
+ti = to
+#
+gts = c(gt0,qrys)
+tg = tibble(gt=gts) %>% mutate(fi=glue("{dirg}/{gt}/50_annotation/15.tsv")) %>%
+    mutate(x = map(fi, read_tsv)) %>%
+    select(gt, x) %>% unnest(x)
+
+tg1 = tg %>% filter(gt==gt0) %>% select(-tid)
+tg2 = ti %>% inner_join(tg, by=c('qry'='gt','gid2'='gid','tid2'='tid')) %>%
+    select(gt=qry,gid=gid1,gid2,type,ttype,etype,chrom,start,end,srd)
+to = tg1 %>% bind_rows(tg2)
+#
+top = to %>% filter(srd=='+')
+tx = top %>% group_by(gt,gid) %>% summarise(tss=min(start)) %>% ungroup()
+top = top %>% inner_join(tx, by=c('gt','gid')) %>%
+    mutate(beg=start-tss+1, end=end-tss+1)
+#
+tom = to %>% filter(srd=='-')
+tx = tom %>% group_by(gt,gid) %>% summarise(tss=max(end)) %>% ungroup()
+tom = tom %>% inner_join(tx, by=c('gt','gid')) %>%
+    mutate(beg=tss-end+1, end=tss-start+1)
+
+to = top %>% bind_rows(tom) %>% select(gt,gid,type=etype,ttype,beg,end,gid2) %>%
+    arrange(gt, gid, type, beg)
+fo = glue("{dirw}/wheat.genes.{v}.rds")
+saveRDS(to, fo)
+#}}}
+#}}}
 
 
